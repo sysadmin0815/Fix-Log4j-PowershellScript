e976850258a7157fd40393290d8a8b5f0bc55a05
sysadmin0815 <81157346+sysadmin0815@users.noreply.github.com>
2021-12-22 23:35:09 +0100

    update to version 1.5

--- Fix-log4j_jndi_7zip.ps1
+++ Fix-log4j_jndi_7zip.ps1
@@ -1,16 +1,19 @@
 # Fix-Log4j_JNDI_7zip.ps1
-# search and remove JNDI class from *log4j*.jar files on the system ( C:\ - drive by default) - can be changed with $log4jFiles
+# search and remove JNDI class from *log4j*.jar files on the system ( C:\ - drive by default)
+# search scope can be changed to search on all local drives with $searchAllDrives set to $true
 #
 # Author: sysadmin0815
 # Date: 16.12.2021
 #############################
-# Mod. Date: 17.12.2021
-# Version 1.4
+# Mod. Date: 22.12.2021
+$scriptVersion = "1.5"
 #Change Log:
 #   added additional if check to stop the process of bk file not found.
 #   added PSScriptRoot for 7zip by default
 #   added $enableBackup
 #   added $searchAllDrives
+#   added verifying process if jndilookup class was removed from jar file
+#
 #############################
 #THE SCRIPT IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND.
 #7zip is used to remove the jndi class Source: https://www.7-zip.org/
@@ -34,30 +37,42 @@ $7zipPath = $PSScriptRoot+"\7-Zip\7za.exe"
 #if set to $false the script will search in $searchpath only for pattern
 $searchAllDrives = $false
 
-#define pattern and search path; default $searchPath = "C:\" 
-$searchPath = "C:\"                     # will be ignored if $searchAllDrives is $true
+#define pattern and search path; default $searchPath = "C:\" ; Always use \ at the end of the path!!
+$searchPath = "C:\log4\"                     # will be ignored if $searchAllDrives is $true
 $filePattern = "*log4j*.jar"
 
 #Enable jar file backup in the same directory before removing class?
-#set to $true to enable backup (recommended)
-#set to $false to disable backup
+#--- set to $true to enable backup (recommended)
+#--- set to $false to disable backup
 $enableBackup =  $true
+
+#Explaination:
+#Each jar file which matches the pattern will be backed up in the same directory as *.bk file before the script
+#removes the class from the jar file. 
+#Then the script searches for the jndilookup class in the jar file to verify it was removed.
+#If the class is still detected after the script ran, the original jar file was not modified.
+#
+#If the validation of jndilookup class removal fails, delete the created backup files to keep the system clean.
+#if the class was NOT removed successfully, the jar file was NOT modified. So keeping the bk file is optional.
+#--- set to $true to delete bk files if the class is still detected in the jar file (default)
+#--- set to $false to keep bk files if the class is still detected in the jar file.
+$removeBkOnFailure = $true          #set to $false if you are not sure
 
 
 # -------------- SCRIPT START ---------------
 
 
 #Test for Admin permissons, if not admin exit script.
-if (! ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "Please run this script with Admin permissions!"
-    exit 1
-}
+#if (! ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+#    Write-Warning "Please run this script with Admin permissions!"
+#    exit 1
+#}
 
 #Get date and time for log
 $date = (Get-Date -Format "ddMMyyyy_HHmm")
 $datelog = (Get-Date -Format "dd.MM.yyyy HH:mm:ss")
 
-#check for existing Log folder
+#Buid LogFolder and LogFile
 $rootFolder = $PSScriptRoot+"\"
 $logFileName="Log4JCleanup.log"
 $folderName = "Log"
@@ -67,7 +82,8 @@ $PathToLogFile = $Path+"\"+$logFileName
 
 #Test for Log Folder if not create it
 if (!(Test-Path $Path)) {
-    New-Item -itemType Directory -Path $PathLogs -Name $folderName -ErrorAction SilentlyContinue
+    Write-Host "[INFO] Creating LogFolder $Path" -ForegroundColor Yellow
+    New-Item -itemType Directory -Path $PathLogs -Name $folderName -ErrorAction SilentlyContinue | Out-Null
 }
 else {
     Write-Host "[INFO] LogFolder already exists." -ForegroundColor Green
@@ -75,13 +91,18 @@ else {
 
 #Test for Log File if not create it
 if (!(Test-Path $PathToLogFile)) {
-    New-Item -itemType File -Path $Path -Name $logFileName -ErrorAction SilentlyContinue
+    Write-Host "[INFO] Creating LogFile $PathToLogFile" -ForegroundColor Yellow
+    New-Item -itemType File -Path $Path -Name $logFileName -ErrorAction SilentlyContinue | Out-Null
     Add-Content -Path $PathToLogFile -Value "$datelog     *** ***    NEW LOG   *** ***   "
+    Add-Content -Path $PathToLogFile -Value "$datelog     Computer Name: $env:computername   "
+    Add-Content -Path $PathToLogFile -Value "$datelog     Script Version: $scriptVersion   "
 }
 else {
     Write-Host "[INFO] LogFile already exists." -ForegroundColor Green
     Add-Content -Path $PathToLogFile -Value ""
     Add-Content -Path $PathToLogFile -Value "$datelog     *** ***    NEW LOG   *** ***   "
+    Add-Content -Path $PathToLogFile -Value "$datelog     Computer Name: $env:computername   "
+    Add-Content -Path $PathToLogFile -Value "$datelog     Script Version: $scriptVersion   "
 }
 
 #Test for 7Zip folder
@@ -91,52 +112,100 @@ if ( ! (Test-Path -Path $7zipPath) ) {
     exit 1
 }
 
+#Output of script version
+Write-Host "[INFO] Script Version: $($scriptVersion)" -ForegroundColor Yellow
+
+#If $searchAllDrives is true
 if ($searchAllDrives) {
+    Write-Host ""
     Write-Host "Searching on all drives. This can take a while..." -ForegroundColor Yellow
     Add-Content -Path $PathToLogFile -Value "$datelog   -- Start search on all drives"
     $log4jFiles = Get-PSDrive -PSProvider FileSystem | ForEach-Object {(Get-ChildItem ($_.Root) -Recurse -Force -Include $filepattern -ErrorAction SilentlyContinue)} |  Where-Object {($_.PSIsContainer -eq $false)} 
 }
+#If $searchAllDrives is false search at $searchPath
 else {
+    Write-Host ""
     Write-Host "Searching in $searchPath . This can take a while..." -ForegroundColor Yellow
     Add-Content -Path $PathToLogFile -Value "$datelog   -- Start search in $searchPath"
     $log4jFiles = get-childitem -Path $searchPath -include $filePattern -File -Force -Recurse -ErrorAction SilentlyContinue | Where-Object {($_.PSIsContainer -eq $false)} 
 }
 
+#If no files with pattern found
 if  (! $log4jFiles -or $log4jFiles.Length -eq 0){
-    Write-Host "No files found matching pattern $filepattern in Path $searchPath" -BackgroundColor Green
-    Add-Content -Path $PathToLogFile -Value "$datelog   -- No files found matching pattern $filepattern in Path $searchPath"
+    Write-Host "No files found matching pattern $filepattern" -ForegroundColor Green
+    Add-Content -Path $PathToLogFile -Value "$datelog   -- No files found matching pattern $filepattern"
     $returnCode = 0
 }
+#If files with pattern found run this part
 else {
     $returnCode = 0
 
-    Write-Host "The folloging files where found:" -ForegroundColor Yellow
+    Write-Host "The following files where found:" -ForegroundColor Yellow
     Write-Output  $log4jFiles.FullName
-    Add-Content -Path $PathToLogFile -Value "$datelog   -- Files found matching pattern $filepattern in Path $searchPath"
+    Write-Host ""
+    Write-Host "Start removal of JNDI Lookup Class" -ForegroundColor Yellow
+    Add-Content -Path $PathToLogFile -Value "$datelog   -- Files found matching pattern $filepattern "
     $log4jFiles.FullName | Add-Content $PathToLogFile
+
+    $tmpFile = "$PSScriptRoot\log4jcleanupTmp.log"
 
     foreach ($file in $log4jFiles) {
         try{ 
             if ($enableBackup) {
+                if (Test-Path -Path $tmpFile) {
+                    Remove-Item -Path $tmpFile -Force
+                }
+
+                #Create backup of jar file
                 $filebkpath = $file.FullName+"_"+$date+".bk"
                 Copy-Item $file.fullname -Destination $filebkpath -Force
-                if (! $filebkpath) {
-                    Write-Host "Error creating backup for file $file" -BackgroundColor Red -ForegroundColor Yellow
-                    Add-Content -Path $PathToLogFile -Value "$datelog   -- Error creating backup for file $file"
-                    $returnCode = 1
+                
+                #Verify backup of jar file exists of not abort and output error
+                if (Test-Path -Path $filebkpath) {
+                    Write-Host "Processing file $file"
+                    Write-Host "   -- Created backup file $filebkPath"
+                    Add-Content -Path $PathToLogFile -Value "$datelog   Backup file to $filebkpath"
+                    Add-Content -Path $PathToLogFile -Value "$datelog   Processing file $file"
+                    & $7zipPath d $file.fullname org/apache/logging/log4j/core/lookup/JndiLookup.class | Out-Null
+                    Write-Host "   -- Checking if JNDILookup Class has been removed"
+                    Start-Process -FilePath "$7zipPath" -ArgumentList "l `"$($file.FullName)`" org/apache/logging/log4j/core/lookup/JndiLookup.class" -NoNewWindow -Wait -RedirectStandardOutput "$tmpFile"
+                    Add-Content -Path $PathToLogFile -Value "$datelog   --- Checking if JNDILookup Class has been removed"
+                    $validate = Select-String -Path "$tmpOutFile" -Pattern "JndiLookup.class" -CaseSensitive -Quiet -SimpleMatch
+                    if (! $validate) {
+                        Write-Host "   -- Verified: File successully cleaned up." -ForegroundColor Green
+                        Add-Content -Path $PathToLogFile -Value "$datelog   --- Verified: File successully cleaned up."
+                    } 
+                    else {
+                        Write-Host "   -- Failure: Check whether you have write permissions or another process is currently using the file." -BackgroundColor Red -ForegroundColor Yellow
+                        Add-Content -Path $PathToLogFile -Value "$datelog   --- Failure: Check whether you have write permissions or another process is currently using the file."
+        
+                        if ($removeBkOnFailure) {
+                            Write-Host "   -- Rollback: Removing backup file $filebkpath" -ForegroundColor Yellow
+                            Add-Content -Path $PathToLogFile -Value "$datelog   --- Rollback: Removing backup file $filebkpath"
+                            Remove-Item $filebkpath
+                        }
+                        $returnCode = 1
+                    }
+
+                    if (Test-Path -Path $tmpFile) {
+                        Remove-Item -Path $tmpFile -Force
+                    }
                 }
                 else{
-                    Add-Content -Path $PathToLogFile -Value "$datelog   -- Backup file to $filebkpath"
-                    Add-Content -Path $PathToLogFile -Value "$datelog   -- Processing file $file"
-                    & $7zipPath d $file.fullname org/apache/logging/log4j/core/lookup/JndiLookup.class
+                    Write-Host "Error creating backup for file $file" -BackgroundColor Red -ForegroundColor Yellow
+                    Write-Host "   -- No changes perfmormed" -BackgroundColor Red -ForegroundColor Yellow
+                    Write-Host ""
+                    Add-Content -Path $PathToLogFile -Value "$datelog   -- Error creating backup for file $file"
+                    Add-Content -Path $PathToLogFile -Value "$datelog      -- No changes performed."
+                    $returnCode = 1
                 }
             }
 
             else{   
                 Add-Content -Path $PathToLogFile -Value "$datelog   -- Backup disabled for file $file"
-                Add-Content -Path $PathToLogFile -Value "$datelog   -- Processing file $file"
+                Add-Content -Path $PathToLogFile -Value "$datelog   --- Processing file $file"
                 & $7zipPath d $file.fullname org/apache/logging/log4j/core/lookup/JndiLookup.class
-                }
+            }
 
         }
         catch{
@@ -147,6 +216,9 @@ else {
         }
     }
 }
+
+Write-Host ""
+Write-Host "The log file is available here: $PathToLogFile" -ForegroundColor Yellow
 
 #comment this line below to not automatically close the script.
 exit $returnCode
